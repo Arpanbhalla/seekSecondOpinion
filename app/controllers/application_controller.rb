882b2378c92666c6eb50344a21b7b9f86e69532a
Arpan B <arpanbhalla@gmail.com>
2016-10-25 20:19:19 +1100

    Fixed footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user,:logged_in?, :is_doctor?, :is_admin?
-  
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -17,13 +17,6 @@ class ApplicationController < ActionController::Base
     if !logged_in?
       flash = "You must be logged in to perform that action"
       redirect_to root_path
-    end
-  end
-
-  def is_doctor?
-    if (User.exists?(:id => session[:user_id]))
-      user = User.find_by(:id => session[:user_id])
-      user.doctor?
     end
   end
 
