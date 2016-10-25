6172ec043ed30b8c40f420a7e6df4c44139c0dc5
Arpan B <arpanbhalla@gmail.com>
2016-10-25 23:08:07 +1100

    Add Doctors to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,16 @@ u1 = User.create :firstname => "admin",
 
 u2 = User.create :firstname => "John",
                  :lastname => "Doe",
-                 :email => "john@ga.com",
+                 :email => "john@gmail.com",
                  :dob => '5/11/1987',
                  :password => "chicken",
                  :password_confirmation => "chicken",
                  :admin => false
 
-u3 = User.create :firstname => "Nicolas",
-                 :lastname => "Cage",
-                 :email => "Nicolas@ga.com",
-                 :dob => '7/01/1964',
+u3 = User.create :firstname => "arpan",
+                 :lastname => "bhalla",
+                 :email => "arpanbhalla@gmail.com",
+                 :dob => '7/01/1984',
                  :password => "chicken",
                  :password_confirmation => "chicken",
                  :admin => false
@@ -39,7 +39,8 @@ d1 = User.create :firstname => "Pooja",
                  Competency management of acute coronary patients",
                  :doctor_professional_experience => "20 yrs",
                  :doctor => true,
-                 :email => "pooja@ga.com",
+                 :email => "pooja@gmail.com",
+                 :image => "http://res.cloudinary.com/dbgvixfy4/image/upload/v1476920422/ngmsdvyasmpdcrrxvy3e.png",
                  :password => "chicken",
                  :password_confirmation => "chicken"
 
@@ -51,6 +52,33 @@ d2 = User.create :firstname => "VK",
                  Well experienced in implantation of eyes",
                  :doctor_professional_experience => "3 yrs",
                  :doctor => true,
-                 :email => "vkv@ga.com",
+                 :email => "vkv@gmail.com",
+                 :image => "http://res.cloudinary.com/dbgvixfy4/image/upload/v1476920390/bkkjd07axnb22nup3glf.jpg",
+                 :password => "chicken",
+                 :password_confirmation => "chicken"
+
+d3 = User.create :firstname => "Sukrit",
+                 :lastname => "Khanna",
+                 :doctor_qualifications =>"( MBBS, MD, DM )",
+                 :doctor_speciality =>"Surgeon",
+                 :doctor_expertise => "More than 10 years of experience as surgeon
+                 Well experienced doctor in surgery",
+                 :doctor_professional_experience => "10 yrs",
+                 :doctor => true,
+                 :email => "sukrit@gmail.com",
+                 :image => "http://res.cloudinary.com/dbgvixfy4/image/upload/v1476956528/ease7wl2qpdnr0iak3tg.jpg",
+                 :password => "chicken",
+                 :password_confirmation => "chicken"
+
+d4 = User.create :firstname => "Sanjana",
+                 :lastname => "Khanna",
+                 :doctor_qualifications =>"( MBBS, MD, DM )",
+                 :doctor_speciality =>"Surgeon",
+                 :doctor_expertise => "More than 7 years of experience as surgeon
+                 Well experienced doctor in surgery",
+                 :doctor_professional_experience => "7 yrs",
+                 :doctor => true,
+                 :email => "sanjana@gmail.com",
+                 :image => "http://res.cloudinary.com/dbgvixfy4/image/upload/v1476956561/fjf6oamc1kwhentk0jyw.jpg",
                  :password => "chicken",
                  :password_confirmation => "chicken"
